b0bd1756dd1783b4847dabb06380add64d77eff4
hrefn <cmcintosh1000@gmail.com>
2022-08-01 09:24:46 -0500

    task 4.8

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { checkToken, extractLocations, getAccessToken, getEvents } from './api';
+import './nprogress.css';
+import WelcomeScreen from './WelcomeScreen';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents />
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    selectedLocation: 'all',
+    showWelcomeScreen: undefined
+  }
+
+  updateEvents = (location, eventCount) => {
+    if (eventCount === undefined) {
+      eventCount = this.state.numberOfEvents
+    } else {
+      this.setState({ numberOfEvents: eventCount });
+    }
+    if (location === undefined) {
+      location = this.state.selectedLocation;
+    }
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all')
+        ? events
+        : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents.slice(0, eventCount),
+        numberOfEvents: eventCount,
+        locationSelected: location,
+      })
+    });
+  };
+
+  async componentDidMount () {
+    this.mounted = true;
+
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+  };
+
+  componentWillUnmount () {
+    this.mounted = false;
+  }
+
+  render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className='App' />
+
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} />
+        <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
+      </div>
+    );
+  }
 }
 
 export default App;
